802aa8f02ce60b3321288b4e269f3f3e1d8f488e
Tyler Fahey <tyler@tylerfahey.com>
2022-03-02 10:55:55 -0600

    Add component scaffolding

---
+++ src/lib/components/FlexContentArea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const FlexContentArea = (props) => {
+   return (
+       <>
+       <div className="flex-content-area">
+           TODO: Make a flex content area here
+       </div>
+      </>
+   )
+}
+export default FlexContentArea;
---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+import FlexContentArea from './components/FlexContentArea';
+import Button from './components/Button';
+export { FlexContentArea, Button };
