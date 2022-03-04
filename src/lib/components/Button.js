9a604a52f76c07c7a2d29c77acc9841dd347e129
Tyler Fahey <tyler@tylerfahey.com>
2022-03-04 15:12:54 -0600

    Add a check for window exists

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import '../styles/build/forty_acres.scss'; // Tell webpack that Button.js uses these styles
+import React from "react";
+import "../styles/build/forty_acres.scss"; // Tell webpack that Button.js uses these styles
 
 const Button = (props) => {
-   return (
-      <button className={`btn btn--${props.kind} CTA background-burnt-orange color-white ${props.classes}`}
+  if (typeof window === "object") {
+    return (
+      <button
+        className={`btn btn--${props.kind} CTA background-burnt-orange color-white ${props.classes}`}
         data-id={props.id}
         type={props.type}
         name={props.name}
         value={props.value}
         disabled={props.disabled}
-        onClick={props.handleClick}> 
-           <h4>{props.label}</h4>
+        onClick={props.handleClick}
+      >
+        <h4>{props.label}</h4>
       </button>
-   )
-}
+    );
+  }
+};
 export default Button;
